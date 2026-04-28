f792654cdf70c295d8ec7ece1886fd4e9da39694
Johannes <hainz.johannes00@gmail.com>
2026-04-28 16:43:50 +0200

    some changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {InputNumber} from 'primeng/inputnumber';
+import {InputText} from 'primeng/inputtext';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputNumber, InputText, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('FlowTrading');
+
+  value = ''
+
 }
